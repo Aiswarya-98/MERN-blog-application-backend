cf87545f501b19d2f93cd2ff9e8a85dfd20a3e7a
Aiswarya-98 <aiswaryasample789@gmail.com>
2024-04-06 23:38:25 +0530

    added netlify live link

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static( __dirname+"/uploads"))
 
 app.use(cors({ 
   credentials: true, 
-  origin: "https://mern-blog-frontend-app.netlify.app/", 
+  origin: "https://mern-blog-frontend-app.netlify.app", 
   methods: "GET,POST,PUT,PATCH,DELETE",
   allowedHeaders: "Content-Type,Authorization"
 }));
