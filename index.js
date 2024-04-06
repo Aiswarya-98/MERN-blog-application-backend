17e81847896303c19076edee618c57373c016374
Aiswarya-98 <aiswaryasample789@gmail.com>
2024-04-07 01:21:09 +0530

    added frontent live link as origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // added render link
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
-app.use(cors({ credentials: true, origin: "https://mern-blog-frontend-h12e.onrender.com/" }))
+app.use(cors({ credentials: true, origin: "https://mern-blog-frontend-h12e.onrender.com" }))
 
 app.use(upload())
 app.use("/uploads", express.static( __dirname+"/uploads"))
