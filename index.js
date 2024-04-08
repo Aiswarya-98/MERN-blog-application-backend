c44f840c91851a070a9c514854747be39702e303
Aiswarya-98 <aiswaryasample789@gmail.com>
2024-04-08 22:48:01 +0530

    added a cache statement

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const postRoutes = require("./routes/postRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
 
 const app = express()
+
+// Middleware to set cache control headers
+app.use((req, res, next) => {
+  // Set cache control headers for images in the /uploads directory
+  if (req.url.startsWith("/uploads")) {
+    res.setHeader("Cache-Control", "public, max-age=604800"); // Cache images for 7 days (604800 seconds)
+  }
+  next();
+});
+
+
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 
