ba245a677fc9c4dca20fafad3b788d5e8dea37f7
Aiswarya-98 <aiswaryasample789@gmail.com>
2024-04-09 10:50:00 +0530

    change in origin

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(express.urlencoded({ extended: true }))
 
 // added render link
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
-app.use(cors({ credentials: true, origin: "https://mern-blog-frontend-h12e.onrender.com" }))
+// app.use(cors({ credentials: true, origin: "https://mern-blog-frontend-h12e.onrender.com" }))
+app.use(cors({credentials:true,origin:"https://mern-blog-application-backend-1.onrender.com"}))
 
 app.use(upload())
 app.use("/uploads", express.static(path.join( __dirname,"/uploads")))
